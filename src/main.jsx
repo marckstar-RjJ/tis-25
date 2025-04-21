501d5ec2beb5c1691f60c596a112091d1e1bb6fa
alejandro <skillparty@outlook.com>
2025-04-21 18:59:12 -0400

    Añadido BrowserRouter para solucionar pantalla en blanco

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
